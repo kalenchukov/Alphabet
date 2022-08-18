31bd7bceb5be716b53524e3c5e8d2a20a71aed32
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 09:12:18 +0300

    Добавлен тагальский алфавит

---
+++ src/main/java/dev/kalenchukov/alphabet/TagalogAlphabet.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Класс букв тагальского алфавита.
+ */
+public class TagalogAlphabet extends AbstractAlphabet implements AlphabeticalCase
+{
+	/**
+	 * Коды букв в Unicode.
+	 */
+	@Unmodifiable
+	@NotNull
+	public static final List<@NotNull Character> LETTERS = List.of(
+			'\u1700', '\u1701', '\u1702', '\u1703', '\u1704', '\u1705', 
+			'\u1706', '\u1707', '\u1708', '\u1709', '\u170A', '\u170B', 
+			'\u170C', '\u170E', '\u170F', '\u1710', '\u1711', '\u1712',
+			'\u1713', '\u1714'
+	);
+
+	/**
+	 * Конструктор для {@code TagalogAlphabet}.
+	 */
+	public TagalogAlphabet()
+	{
+		super(TagalogAlphabet.LETTERS);
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(@Nullable final Object obj)
+	{
+		if (obj == null) {
+			return false;
+		}
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final TagalogAlphabet alphabet = (TagalogAlphabet) obj;
+
+		if (!Objects.equals(TagalogAlphabet.LETTERS, alphabet.get())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return TagalogAlphabet.LETTERS.hashCode();
+	}
+}
