d1c4eb8cd75535e962e2270efbe8aeabc1fccc19
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 10:06:38 +0300

    Добавлен алфавит хануноо

---
+++ src/main/java/dev/kalenchukov/alphabet/HanunooAlphabet.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Класс букв алфавита хануноо.
+ */
+public class HanunooAlphabet extends AbstractAlphabet implements AlphabeticalCase
+{
+	/**
+	 * Коды букв в Unicode.
+	 */
+	@Unmodifiable
+	@NotNull
+	public static final List<@NotNull Character> LETTERS = List.of(
+			'\u1720', '\u1721', '\u1722', '\u1723', '\u1724', '\u1725', 
+			'\u1726', '\u1727', '\u1728', '\u1729', '\u172A', '\u172B', 
+			'\u172C', '\u172D', '\u172E', '\u172F', '\u1730', '\u1731', 
+			'\u1732', '\u1733', '\u1734', '\u1735', '\u1736'
+	);
+
+	/**
+	 * Конструктор для {@code HanunooAlphabet}.
+	 */
+	public HanunooAlphabet()
+	{
+		super(HanunooAlphabet.LETTERS);
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(@Nullable final Object obj)
+	{
+		if (obj == null) {
+			return false;
+		}
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final HanunooAlphabet alphabet = (HanunooAlphabet) obj;
+
+		if (!Objects.equals(HanunooAlphabet.LETTERS, alphabet.get())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return HanunooAlphabet.LETTERS.hashCode();
+	}
+}
