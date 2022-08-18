9cce4f04e982ba0422047a29dcdcfe6e6e310921
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 09:15:43 +0300

    Добавлен алфавит тагбанва

---
+++ src/main/java/dev/kalenchukov/alphabet/TagbanwaAlphabet.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Класс букв алфавита тагбанва.
+ */
+public class TagbanwaAlphabet extends AbstractAlphabet implements AlphabeticalCase
+{
+	/**
+	 * Коды букв в Unicode.
+	 */
+	@Unmodifiable
+	@NotNull
+	public static final List<@NotNull Character> LETTERS = List.of(
+			'\u1760', '\u1761', '\u1762', '\u1763', '\u1764', '\u1765', 
+			'\u1766', '\u1767', '\u1768', '\u1769', '\u176A', '\u176B', 
+			'\u176C', '\u176E', '\u176F', '\u1770', '\u1772', '\u1773'
+	);
+
+	/**
+	 * Конструктор для {@code TagbanwaAlphabet}.
+	 */
+	public TagbanwaAlphabet()
+	{
+		super(TagbanwaAlphabet.LETTERS);
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(@Nullable final Object obj)
+	{
+		if (obj == null) {
+			return false;
+		}
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final TagbanwaAlphabet alphabet = (TagbanwaAlphabet) obj;
+
+		if (!Objects.equals(TagbanwaAlphabet.LETTERS, alphabet.get())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return TagbanwaAlphabet.LETTERS.hashCode();
+	}
+}
