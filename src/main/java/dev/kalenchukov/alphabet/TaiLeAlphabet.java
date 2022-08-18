d8e273504a795cea1d1955c792acb0267d25bb40
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 09:18:02 +0300

    Добавлен алфавит лы

---
+++ src/main/java/dev/kalenchukov/alphabet/TaiLeAlphabet.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Класс букв алфавита лы.
+ */
+public class TaiLeAlphabet extends AbstractAlphabet implements AlphabeticalCase
+{
+	/**
+	 * Коды букв в Unicode.
+	 */
+	@Unmodifiable
+	@NotNull
+	public static final List<@NotNull Character> LETTERS = List.of(
+			'\u1950', '\u1951', '\u1952', '\u1953', '\u1954', '\u1955', 
+			'\u1956', '\u1957', '\u1958', '\u1959', '\u195A', '\u195B', 
+			'\u195C', '\u195D', '\u195E', '\u195F', '\u1960', '\u1961', 
+			'\u1962', '\u1963', '\u1964', '\u1965', '\u1966', '\u1967', 
+			'\u1968', '\u1969', '\u196A', '\u196B', '\u196C', '\u196D', 
+			'\u1970', '\u1971', '\u1972', '\u1973', '\u1974'
+	);
+
+	/**
+	 * Конструктор для {@code TaiLeAlphabet}.
+	 */
+	public TaiLeAlphabet()
+	{
+		super(TaiLeAlphabet.LETTERS);
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(@Nullable final Object obj)
+	{
+		if (obj == null) {
+			return false;
+		}
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final TaiLeAlphabet alphabet = (TaiLeAlphabet) obj;
+
+		if (!Objects.equals(TaiLeAlphabet.LETTERS, alphabet.get())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return TaiLeAlphabet.LETTERS.hashCode();
+	}
+}
