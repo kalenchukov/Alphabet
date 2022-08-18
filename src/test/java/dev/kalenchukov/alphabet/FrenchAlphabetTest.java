cf2ea8706b09f629309641a918a45a3ad103cc5c
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 08:07:22 +0300

    Добавлен французский алфавит

---
+++ src/test/java/dev/kalenchukov/alphabet/FrenchAlphabetTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright © 2022 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FrenchAlphabetTest
+{
+	@NotNull
+	public static final Alphabetical ALPHABET = new FrenchAlphabet();
+
+	/**
+	 * Проверяет получение букв алфавита.
+	 */
+	@Test
+	public void testGet()
+	{
+		List<Character> letters = List.of(
+			'A', 'a', 'B', 'b', 'C', 'c', 
+			'D', 'd', 'E', 'e', 'F', 'f', 
+			'G', 'g', 'H', 'h', 'I', 'i', 
+			'J', 'j', 'K', 'k', 'L', 'l', 
+			'M', 'm', 'N', 'n', 'O', 'o', 
+			'P', 'p', 'Q', 'q', 'R', 'r', 
+			'S', 's', 'T', 't', 'U', 'u', 
+			'V', 'v', 'W', 'w', 'X', 'x', 
+			'Y', 'y', 'Z', 'z'
+		);
+
+		assertArrayEquals(letters.toArray(), ALPHABET.get().toArray());
+	}
+
+	/**
+	 * Проверка сравнения объектов.
+	 */
+	@Test
+	public void testEquals()
+	{
+		assertFalse(ALPHABET.equals(null));
+
+		assertTrue(ALPHABET.equals(ALPHABET));
+
+		assertFalse(ALPHABET.equals(new RussianAlphabet()));
+
+		assertTrue(ALPHABET.equals(new FrenchAlphabet()));
+	}
+
+	/**
+	 * Проверка хэш-кода.
+	 */
+	@Test
+	public void testHashCode()
+	{
+		assertEquals(ALPHABET.hashCode(), ALPHABET.hashCode());
+
+		assertEquals(ALPHABET.hashCode(), new FrenchAlphabet().hashCode());
+
+		assertNotEquals(ALPHABET.hashCode(), new RussianAlphabet().hashCode());
+	}
+}
