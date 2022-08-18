c68289a3ce75ba9e5b8f81cbe983749b067735a7
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 10:02:08 +0300

    Добавлен алфавит пиньинь

---
+++ src/main/java/dev/kalenchukov/alphabet/ChineseAlphabet.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Класс букв пиньинь алфавита.
+ */
+public class ChineseAlphabet extends AbstractAlphabet implements AlphabeticalCase
+{
+	/**
+	 * Коды букв в Unicode.
+	 */
+	@Unmodifiable
+	@NotNull
+	public static final List<@NotNull Character> LETTERS = List.of(
+			'\u0041', '\u0061', '\u0042', '\u0062', '\u0043', '\u0063', 
+			'\u0044', '\u0064', '\u0045', '\u0065', '\u0046', '\u0066',
+			'\u0047', '\u0067', '\u0048', '\u0068', '\u0069', '\u004A',
+			'\u006A', '\u004B', '\u006B', '\u004C', '\u006C', '\u004D',
+			'\u006D', '\u004E', '\u006E', '\u004F', '\u006F', '\u0050',
+			'\u0070', '\u0051', '\u0071', '\u0052', '\u0072', '\u0053',
+			'\u0073', '\u0054', '\u0074', '\u0055', '\u0075', '\u00DC',
+			'\u00FC', '\u0057', '\u0077', '\u0058', '\u0078', '\u0059',
+			'\u0079', '\u005A', '\u007A'
+	);
+
+	/**
+	 * Конструктор для {@code ChineseAlphabet}.
+	 */
+	public ChineseAlphabet()
+	{
+		super(ChineseAlphabet.LETTERS);
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(@Nullable final Object obj)
+	{
+		if (obj == null) {
+			return false;
+		}
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final ChineseAlphabet alphabet = (ChineseAlphabet) obj;
+
+		if (!Objects.equals(ChineseAlphabet.LETTERS, alphabet.get())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return ChineseAlphabet.LETTERS.hashCode();
+	}
+
+	/**
+	 * Класс прописных букв пиньинь алфавита.
+	 */
+	public static class UpperCase extends AbstractAlphabet implements AlphabeticalUpperCase
+	{
+		/**
+		 * Коды прописных букв в Unicode.
+		 */
+		@Unmodifiable
+		@NotNull
+		public static final List<@NotNull Character> LETTERS = List.of(
+			'\u0041', '\u0042', '\u0043', '\u0044', '\u0045', '\u0046', 
+			'\u0047', '\u0048', '\u004A', '\u004B', '\u004C', '\u004D', 
+			'\u004E', '\u004F', '\u0050', '\u0051', '\u0052', '\u0053', 
+			'\u0054', '\u0055', '\u00DC', '\u0057', '\u0058', '\u0059', 
+			'\u005A'
+		);
+
+		/**
+		 * Конструктор для {@code ChineseAlphabet.UpperCase}.
+		 */
+		public UpperCase()
+		{
+			super(ChineseAlphabet.UpperCase.LETTERS);
+		}
+
+		/**
+		 * @see Object#equals(Object)
+		 */
+		@Override
+		public boolean equals(@Nullable final Object obj)
+		{
+			if (obj == null) {
+				return false;
+			}
+
+			if (this == obj) {
+				return true;
+			}
+
+			if (this.getClass() != obj.getClass()) {
+				return false;
+			}
+
+			final ChineseAlphabet.UpperCase alphabet = (ChineseAlphabet.UpperCase) obj;
+
+			if (!Objects.equals(ChineseAlphabet.UpperCase.LETTERS, alphabet.get())) {
+				return false;
+			}
+
+			return true;
+		}
+
+		/**
+		 * @see Object#hashCode()
+		 */
+		@Override
+		public int hashCode()
+		{
+			return ChineseAlphabet.LETTERS.hashCode();
+		}
+	}
+
+	/**
+	 * Класс строчных букв пиньинь алфавита.
+	 */
+	public static class LowerCase extends AbstractAlphabet implements AlphabeticalLowerCase
+	{
+		/**
+		 * Коды строчных букв в Unicode.
+		 */
+		@Unmodifiable
+		@NotNull
+		public static final List<@NotNull Character> LETTERS = List.of(
+			'\u0061', '\u0062', '\u0063', '\u0064', '\u0065', '\u0066', 
+			'\u0067', '\u0068', '\u0069', '\u006A', '\u006B', '\u006C', 
+			'\u006D', '\u006E', '\u006F', '\u0070', '\u0071', '\u0072', 
+			'\u0073', '\u0074', '\u0075', '\u00FC', '\u0077', '\u0078', 
+			'\u0079', '\u007A'
+		);
+
+		/**
+		 * Конструктор для {@code ChineseAlphabet.LowerCase}.
+		 */
+		public LowerCase()
+		{
+			super(ChineseAlphabet.LowerCase.LETTERS);
+		}
+
+		/**
+		 * @see Object#equals(Object)
+		 */
+		@Override
+		public boolean equals(@Nullable final Object obj)
+		{
+			if (obj == null) {
+				return false;
+			}
+
+			if (this == obj) {
+				return true;
+			}
+
+			if (this.getClass() != obj.getClass()) {
+				return false;
+			}
+
+			final ChineseAlphabet.LowerCase alphabet = (ChineseAlphabet.LowerCase) obj;
+
+			if (!Objects.equals(ChineseAlphabet.LowerCase.LETTERS, alphabet.get())) {
+				return false;
+			}
+
+			return true;
+		}
+
+		/**
+		 * @see Object#hashCode()
+		 */
+		@Override
+		public int hashCode()
+		{
+			return ChineseAlphabet.LETTERS.hashCode();
+		}
+	}
+}
---
+++ src/test/java/dev/kalenchukov/alphabet/ChineseAlphabetTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ChineseAlphabetTest
+{
+	@NotNull
+	public static final Alphabetical ALPHABET = new ChineseAlphabet();
+
+	/**
+	 * Проверяет получение букв алфавита.
+	 */
+	@Test
+	public void testGet()
+	{
+		List<Character> letters = List.of(
+			'A', 'a', 'B', 'b', 'C', 'c', 
+			'D', 'd', 'E', 'e', 'F', 'f', 
+			'G', 'g', 'H', 'h', 'i', 'J', 
+			'j', 'K', 'k', 'L', 'l', 'M', 
+			'm', 'N', 'n', 'O', 'o', 'P', 
+			'p', 'Q', 'q', 'R', 'r', 'S', 
+			's', 'T', 't', 'U', 'u', 'Ü', 
+			'ü', 'W', 'w', 'X', 'x', 'Y', 
+			'y', 'Z', 'z'
+		);
+
+		assertArrayEquals(letters.toArray(), ALPHABET.get().toArray());
+	}
+
+	/**
+	 * Проверка сравнения объектов.
+	 */
+	@Test
+	public void testEquals()
+	{
+		assertFalse(ALPHABET.equals(null));
+
+		assertTrue(ALPHABET.equals(ALPHABET));
+
+		assertFalse(ALPHABET.equals(new RussianAlphabet()));
+
+		assertTrue(ALPHABET.equals(new ChineseAlphabet()));
+	}
+
+	/**
+	 * Проверка хэш-кода.
+	 */
+	@Test
+	public void testHashCode()
+	{
+		assertEquals(ALPHABET.hashCode(), ALPHABET.hashCode());
+
+		assertEquals(ALPHABET.hashCode(), new ChineseAlphabet().hashCode());
+
+		assertNotEquals(ALPHABET.hashCode(), new RussianAlphabet().hashCode());
+	}
+}
