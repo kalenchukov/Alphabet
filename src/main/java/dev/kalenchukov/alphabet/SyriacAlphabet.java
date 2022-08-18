882c2d7feb01b2c25a5efd9b25436a035b4a1de5
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 09:12:58 +0300

    Добавлен сирийский алфавит

---
+++ src/main/java/dev/kalenchukov/alphabet/SyriacAlphabet.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Класс букв сирийского алфавита.
+ */
+public class SyriacAlphabet extends AbstractAlphabet implements AlphabeticalCase
+{
+	/**
+	 * Коды букв в Unicode.
+	 */
+	@Unmodifiable
+	@NotNull
+	public static final List<@NotNull Character> LETTERS = List.of(
+			'\u0710', '\u0712', '\u0713', '\u0715', '\u0717', '\u0718', 
+			'\u0719', '\u071A', '\u071B', '\u071D', '\u071F', '\u0720', 
+			'\u0721', '\u0722', '\u0723', '\u0725', '\u0726', '\u0728', 
+			'\u0729', '\u072A', '\u072B', '\u072C'
+	);
+
+	/**
+	 * Конструктор для {@code SyriacAlphabet}.
+	 */
+	public SyriacAlphabet()
+	{
+		super(SyriacAlphabet.LETTERS);
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(@Nullable final Object obj)
+	{
+		if (obj == null) {
+			return false;
+		}
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final SyriacAlphabet alphabet = (SyriacAlphabet) obj;
+
+		if (!Objects.equals(SyriacAlphabet.LETTERS, alphabet.get())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return SyriacAlphabet.LETTERS.hashCode();
+	}
+}
