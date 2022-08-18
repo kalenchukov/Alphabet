b8c32079565f63b01e3fa3058a534c5085102353
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 08:10:49 +0300

    Добавлен латинский алфавит

---
+++ src/test/java/dev/kalenchukov/alphabet/LatinAlphabetLowerCaseTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2022 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LatinAlphabetLowerCaseTest
+{
+	@NotNull
+	public static final Alphabetical ALPHABET = new LatinAlphabet.LowerCase();
+
+	/**
+	 * Проверяет получение букв алфавита.
+	 */
+	@Test
+	public void testGet()
+	{
+		List<Character> letters = List.of(
+			'a', 'b', 'c', 'd', 'e', 'f', 
+			'g', 'h', 'i', 'j', 'k', 'l', 
+			'm', 'n', 'o', 'p', 'q', 'r', 
+			's', 't', 'u', 'v', 'w', 'x', 
+			'y', 'z'
+		);
+
+		assertArrayEquals(letters.toArray(), ALPHABET.get().toArray());
+	}
+
+	/**
+	 * Проверка сравнения объектов.
+	 */
+	@Test
+	public void testEquals()
+	{
+		assertFalse(ALPHABET.equals(null));
+
+		assertTrue(ALPHABET.equals(ALPHABET));
+
+		assertFalse(ALPHABET.equals(new RussianAlphabet.LowerCase()));
+
+		assertTrue(ALPHABET.equals(new LatinAlphabet.LowerCase()));
+	}
+
+	/**
+	 * Проверка хэш-кода.
+	 */
+	@Test
+	public void testHashCode()
+	{
+		assertEquals(ALPHABET.hashCode(), ALPHABET.hashCode());
+
+		assertEquals(ALPHABET.hashCode(), new LatinAlphabet.LowerCase().hashCode());
+
+		assertNotEquals(ALPHABET.hashCode(), new RussianAlphabet.LowerCase().hashCode());
+	}
+}
