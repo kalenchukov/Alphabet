880268bcfe9f680fe51f8149d2d4b76e69d88524
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 09:24:21 +0300

    Добавлен огамический алфавит

---
+++ src/main/java/dev/kalenchukov/alphabet/OghamAlphabet.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Класс букв огамического алфавита.
+ */
+public class OghamAlphabet extends AbstractAlphabet implements AlphabeticalCase
+{
+	/**
+	 * Коды букв в Unicode.
+	 */
+	@Unmodifiable
+	@NotNull
+	public static final List<@NotNull Character> LETTERS = List.of(
+			'\u1680', '\u1681', '\u1682', '\u1683', '\u1684', '\u1685', 
+			'\u1686', '\u1687', '\u1688', '\u1689', '\u168A', '\u168B', 
+			'\u168C', '\u168D', '\u168E', '\u168F', '\u1690', '\u1691', 
+			'\u1692', '\u1693', '\u1694', '\u1695', '\u1696', '\u1697', 
+			'\u1698', '\u1699', '\u169A', '\u169B', '\u169C'
+	);
+
+	/**
+	 * Конструктор для {@code OghamAlphabet}.
+	 */
+	public OghamAlphabet()
+	{
+		super(OghamAlphabet.LETTERS);
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(@Nullable final Object obj)
+	{
+		if (obj == null) {
+			return false;
+		}
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final OghamAlphabet alphabet = (OghamAlphabet) obj;
+
+		if (!Objects.equals(OghamAlphabet.LETTERS, alphabet.get())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return OghamAlphabet.LETTERS.hashCode();
+	}
+}
