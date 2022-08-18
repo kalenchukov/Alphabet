f3fb33d0b1010ce64ef08583c30b3f04c8cc2350
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-08-18 09:09:14 +0300

    Добавлен алфавит бухид

---
+++ src/main/java/dev/kalenchukov/alphabet/BuhidAlphabet.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2022 Алексей Каленчуков
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Класс букв алфавита бухид.
+ */
+public class BuhidAlphabet extends AbstractAlphabet implements AlphabeticalCase
+{
+	/**
+	 * Коды букв в Unicode.
+	 */
+	@Unmodifiable
+	@NotNull
+	public static final List<@NotNull Character> LETTERS = List.of(
+			'\u1740', '\u1741', '\u1742', '\u1743', '\u1744', '\u1745', 
+			'\u1746', '\u1747', '\u1748', '\u1749', '\u174A', '\u174B', 
+			'\u174C', '\u174D', '\u174E', '\u174F', '\u1750', '\u1751', 
+			'\u1752', '\u1753'
+	);
+
+	/**
+	 * Конструктор для {@code BuhidAlphabet}.
+	 */
+	public BuhidAlphabet()
+	{
+		super(BuhidAlphabet.LETTERS);
+	}
+
+	/**
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(@Nullable final Object obj)
+	{
+		if (obj == null) {
+			return false;
+		}
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final BuhidAlphabet alphabet = (BuhidAlphabet) obj;
+
+		if (!Objects.equals(BuhidAlphabet.LETTERS, alphabet.get())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @see Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return BuhidAlphabet.LETTERS.hashCode();
+	}
+}
