a28da1d9725f71881ab7b24617d58d60f7f71125
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2023-02-12 13:14:58 +0300

    Добавлен класс AlphabetTest

---
+++ src/test/java/dev/kalenchukov/alphabet/resources/AlphabetTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2023 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.kalenchukov.alphabet.resources;
+
+import dev.kalenchukov.alphabet.RussianAlphabet;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Класс проверки констант и методов перечисления {@link Alphabet}.
+ */
+class AlphabetTest
+{
+	/**
+	 * Проверка метода {@link Alphabet#getAlphabet()}.
+	 */
+	@Test
+	void getAlphabet()
+	{
+		assertEquals(new RussianAlphabet(), Alphabet.RUSSIAN.getAlphabet());
+	}
+
+	/**
+	 * Проверка метода {@link Alphabet#toString()}.
+	 */
+	@Test
+	void testToString()
+	{
+		assertEquals("SYLOTI NAGRI", Alphabet.SYLOTI_NAGRI.toString());
+	}
+}
